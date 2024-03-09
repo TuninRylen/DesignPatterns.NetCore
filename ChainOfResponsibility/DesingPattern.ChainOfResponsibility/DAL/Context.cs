27d10f8b09ce814c292c45c9b0878a6df74cb76e
TuninRylen <132095456+TuninRylen@users.noreply.github.com>
2024-03-09 13:33:56 +0300

    v1-ChainOfResponsibility

---
+++ ChainOfResponsibility/DesingPattern.ChainOfResponsibility/DAL/Context.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DesingPattern.ChainOfResponsibility.DAL
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer();
+        }
+    }
+}
