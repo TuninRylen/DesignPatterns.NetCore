96969d349472a1a732d1282b741d7acc898ada07
TuninRylen <132095456+TuninRylen@users.noreply.github.com>
2024-03-09 13:35:22 +0300

    delete

--- ChainOfResponsibility/DesingPattern.ChainOfResponsibility/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DesingPattern.ChainOfResponsibility.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
